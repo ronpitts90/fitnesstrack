b169e334bcfc0918e15071eb8c0e089c22b69bf0
Ronald Pitts <ronpitts81@gmail.com>
2020-10-13 19:25:24 -0400

    idk

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -5,7 +5,9 @@ module.exports = function(app) {
   app.get("/exercise", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
-  // Not quite sure what this is used for yet ....
+
+
+  
   app.get("/stats", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
